cc6ff9aba7bd6c3fa34566793ce717d46a45a104
handerman <2862551221@qq.com>
2019-04-01 10:59:22 +0800

    字符数组变字符串

---
+++ java_project/src/project_2.java
@@ -0,0 +1,12 @@
+
+
+
+public class project_2 {
+    public static void main(String[] args){
+     char str [] = new char[]{'h','e','l','l','o'};
+        System.out.println(new String(str));
+        for(int i = 0;i < 5;i++) {
+            System.out.println(str[i]);
+        }
+    }
+}
