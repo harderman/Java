6590c5d1ad6c93f4f3aa165c3d0bbf7637a303ed
handerman <2862551221@qq.com>
2019-04-01 11:46:16 +0800

    text

--- java_project/src/project_2.java
+++ java_project/src/project_2.java
@@ -1,12 +1,19 @@
 
+public class project_2
+{
+    public static void main(String[] args)
+    {
+        //char str [] = new char[]{'h','e','l','l','o'};
+        String str = "hello world";
+        //System.out.println(str.charAt(10));
+        char Str[] = str.toCharArray();
 
+       for (int i = 0; i < 11; i++)
+       {
+           System.out.println(Str[i]);
+       }
 
-public class project_2 {
-    public static void main(String[] args){
-     char str [] = new char[]{'h','e','l','l','o'};
-        System.out.println(new String(str));
-        for(int i = 0;i < 5;i++) {
-            System.out.println(str[i]);
-        }
     }
 }
+
+
