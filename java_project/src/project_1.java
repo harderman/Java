88111169abd0fff0384d011129f4487a7eba12de
handerman <2862551221@qq.com>
2019-04-03 19:17:40 +0800

    字符串变字符数组

---
+++ java_project/src/project_1.java
@@ -0,0 +1,10 @@
+public class project_1
+{
+    public static void main(String[] args)
+    {
+        char[] str1 = new char[]{'h','e','l','l','o'};
+        System.out.println(new String(str1,0,5));
+        String str = "world";
+        System.out.println(str.toCharArray());
+    }
+}
---
+++ java_project/src/text.java
@@ -0,0 +1,9 @@
+
+
+public class text
+{
+    public static void main(String[] args)
+    {
+        StringBuffer sb = new StringBuffer();
+    }
+}
