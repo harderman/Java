10ffc14d1606c93a5243d9d3bc2f1a34157f9621
handerman <2862551221@qq.com>
2019-04-03 19:22:19 +0800

    String Buffer类的append方法

--- java_project/src/text.java
+++ java_project/src/text.java
@@ -5,5 +5,6 @@ public class text
     public static void main(String[] args)
     {
         StringBuffer sb = new StringBuffer();
+        System.out.println(sb.append("hello").append(" ").append("world"));
     }
 }
