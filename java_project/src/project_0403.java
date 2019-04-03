05f1f646a9d07c9b7006e103104d6f9f8621bfd7
handerman <2862551221@qq.com>
2019-04-03 19:31:09 +0800

    StringBuffer的reverse方法，字符串反转

---
+++ java_project/src/project_0403.java
@@ -0,0 +1,9 @@
+public class project_0403
+{
+    public static void main(String[] args)
+    {
+        StringBuffer sb = new StringBuffer("hello world");
+        System.out.println(sb.reverse());
+    }
+
+}
