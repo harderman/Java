af22461b261e4f65d0fb9f21dc7173361e714ed9
handerman <2862551221@qq.com>
2019-04-07 22:24:32 +0800

    foreach循环

---
+++ java_project/src/java_project/for_each.java
@@ -0,0 +1,10 @@
+package java_project;
+
+public class for_each {
+    public static void main(String[] args) {
+        int [] data = new int[]{1,3,5,7,9};
+        for(int i:data){
+            System.out.println(i);
+        }
+    }
+}
