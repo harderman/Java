fca3a211a951262f8c1ff3a984bea08ab8b9e238
handerman <2862551221@qq.com>
2019-04-08 16:15:50 +0800

    泛型

---
+++ java_project/src/java_project/Text_1.java
@@ -0,0 +1,33 @@
+package java_project;
+
+import java.lang.reflect.Field;
+
+class MyClass<T,E>{
+    private T message;
+    private E text;
+    public E getText() {
+        return text;
+    }
+    public void setText(E text) {
+        this.text = text;
+    }
+    public T getMessage() {
+        return message;
+    }
+    public void setMessage(T message) {
+        this.message = message;
+    }
+    public void testMethod1(T t) {
+        System.out.println(t);
+    }
+}
+    public class Text_1{
+    public static void main(String[] args) {
+        MyClass<String,Integer> myClass1 = new MyClass<>();
+        Class cls = myClass1.getClass();
+        Field[] fields = cls.getDeclaredFields();
+        for (Field field : fields) {
+        System.out.println(field.getType());
+        }
+    }
+}
