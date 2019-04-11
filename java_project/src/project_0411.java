1d2cb6f82ae2097165fc69a7948b3e4ed260e399
handerman <2862551221@qq.com>
2019-04-11 15:56:29 +0800

    Java泛型之通配符

---
+++ java_project/src/project_0411.java
@@ -0,0 +1,23 @@
+
+class Message<T>{
+    private T massage;
+
+    public void setMassage(T massage) {
+        this.massage = massage;
+    }
+
+    public T getMassage() {
+        return massage;
+    }
+}
+public class project_0411{
+    public static void main(String[] args) {
+        Message<Integer> message = new Message();
+        message.setMassage(55);
+        fun(message);
+    }
+    public static void fun(Message<?> temp){
+        //temp.setMassage(100);
+        System.out.println(temp.getMassage());
+    }
+}
