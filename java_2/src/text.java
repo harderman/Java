fce27254be5703ad542cfe923cb9743e4aac4685
handerman <2862551221@qq.com>
2019-04-18 16:14:21 +0800

    反射实例化对象

--- java_2/src/text.java
+++ java_2/src/text.java
@@ -5,12 +5,15 @@ class Learn{
     }
 }
 public class text {
-    public static void main(String[] args) throws ClassNotFoundException {
+    public static void main(String[] args) throws ClassNotFoundException,InstantiationException
+    ,IllegalAccessException{
         Learn learn = new Learn();
         learn.means();
         System.out.println(learn.getClass());
         System.out.println(Learn.class);
         Class<?> learn1 = Class.forName("java.util.Date");
         System.out.println(learn1.getName());
+        Learn learn2 = (Learn)Class.forName("Learn").newInstance();//反射实例化Learn的对象
+        learn2.means();
     }
 }
