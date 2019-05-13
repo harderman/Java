218ec05e9b28c812d8fa11efb456aaeedd1a3d69
handerman <2862551221@qq.com>
2019-05-13 17:55:22 +0800

    无头节点链表

--- java_2/src/xxx.java
+++ java_2/src/xxx.java
@@ -46,50 +46,130 @@ public class xxx  implements ILinked{
         }
     }
 
-//    private Node searchIndex(){
-//
-//    }
-//    private Node checkIndex(int index){
-//        Node cur;
-//        if(index < 0 || index > getLength()) {
-//
-//        }
-//        return cur;
-//    }
-
 //指定位置插入
+//    @Override
+//    public boolean addIndex(int index, int data) {
+//        Node node = new Node(data);
+//        Node Located = this.head;
+//        Node Locate = this.head;
+//        //index为头时
+//        if(index == 0){
+//            addFirst(data);
+//            return true;
+//        //index为尾时
+//    } else if(index == getLength() - 1){
+//            addLast(data);
+//            return true;
+//        //index非法时
+//    } else if(index < 0 || index > getLength()){
+//            return false;
+//        //插入一般情况下的index
+//    } else{
+//            int location = 1;
+//            Located = Located.next;
+//            while(index != location){
+//                Located = Located.next;
+//                Locate = Locate.next;
+//                location++;
+//            }
+//            node.next = Located;
+//            Locate.next = node;
+//            return true;
+//        }
+//       // return false;
+//    }
+////删除指定位置
+//    @Override
+//    public int remove(int key) {
+//        Node cur = this.head;
+//        Node Located = this.head;
+//        Node Locate = this.head;
+//        //key非法时
+//        if(key < 0||key > getLength()){
+//            return -1;
+//        }
+//        else if(key == 0){
+//           this.head = this.head.next;
+//           return 0;
+//        }
+//        else if(key == getLength()-1){
+//            for(int i =0;i<getLength()-2;i++){
+//                cur = cur.next;
+//            }
+//            cur.next = null;
+//            return 0;
+//        }
+//        else{
+//            int location = 1;
+//            Located = Located.next;
+//            Located = Located.next;
+//            while(key != location) {
+//                Located = Located.next;
+//                Locate = Locate.next;
+//                location++;
+//            }
+//            Locate.next = Located;
+//        }
+//        return 0;
+//    }
+////删除指定的所有内容
+//    @Override
+//    public void removeAllKey(int key) {
+//       Node prev = this.head;
+//       Node cur = this.head.next;
+//       while(cur != null){
+//           if(cur.data == key){
+//               prev.next = cur.next;
+//               cur = cur.next;
+//           } else{
+//               prev = cur;
+//               cur = cur.next;
+//           }
+//       }
+//    }
+//找前驱
+    private Node SearchIndex(int index){
+        checkIndex(index);
+        Node cur = this.head;
+        int count = 0;
+        while(count < index - 1){
+            cur = cur.next;
+            count++;
+        }
+        return cur;
+    }
+//找前驱
+    private Node SearchPrev(int key){
+        Node cur = this.head;
+        while(cur.next != null){
+            if(cur.next.data == key){
+                return cur;
+            }
+            cur = cur.next;
+        }
+        return null;
+    }
+//检测index是否合法
+    private void checkIndex(int index){
+        if(index < 0||index > getLength()){
+            throw new UnsupportedOperationException("非法的index");
+        }
+    }
+
     @Override
     public boolean addIndex(int index, int data) {
-        Node node = new Node(data);
-        Node Located = this.head;
-        Node Locate = this.head;
-        //index为头时
+        checkIndex(index);
         if(index == 0){
             addFirst(data);
             return true;
-        //index为尾时
-    } else if(index == getLength() - 1){
-            addLast(data);
-            return true;
-        //index非法时
-    } else if(index < 0 || index > getLength()){
-            return false;
-        //插入一般情况下的index
-    } else{
-            int location = 1;
-            Located = Located.next;
-            while(index != location){
-                Located = Located.next;
-                Locate = Locate.next;
-                location++;
-            }
-            node.next = Located;
-            Locate.next = node;
-            return true;
         }
-       // return false;
+        Node node = new Node(data);
+        Node before = SearchIndex(index);
+        node.next = before.next;
+        before.next = node;
+        return true;
     }
-//是否包含指定元素
+
     @Override
     public boolean contains(int key) {
         Node cur = this.head;
@@ -99,73 +179,52 @@ public class xxx  implements ILinked{
             }
             cur = cur.next;
         }
+
         return false;
     }
-//        for(int i = 0;i<getLength();i++){
-//            if (this.head.data != key) {
-//                this.head = this.head.next;
-//            } else {
-//                return true;
-//            }
-//        }
-//        return false;
 
-//删除指定位置
     @Override
     public int remove(int key) {
-        Node cur = this.head;
-        Node Located = this.head;
-        Node Locate = this.head;
-        //key非法时
-        if(key < 0||key > getLength()){
-            return -1;
+//单链表为空
+        if(this.head == null ){
+            throw new UnsupportedOperationException("单链表为空");
         }
-        else if(key == 0){
-           this.head = this.head.next;
-           return 0;
+//key在头节点
+        if(this.head.data == key ){
+            this.head = this.head.next;
+            return key;
         }
-        else if(key == getLength()-1){
-            for(int i =0;i<getLength()-2;i++){
-                cur = cur.next;
-            }
-            cur.next = null;
-            return 0;
+        if( SearchPrev(key) == null){
+            throw new UnsupportedOperationException("没有前驱");
         }
-        else{
-            int location = 1;
-            Located = Located.next;
-            Located = Located.next;
-            while(key != location) {
-                Located = Located.next;
-                Locate = Locate.next;
-                location++;
-            }
-            Locate.next = Located;
-        }
-        /**删除指定内容
-         * while(cur.next != null){
-         *
-         * }
-         *
-         */
-        return 0;
+        Node Prev = SearchPrev(key);
+        Node delete = Prev.next;
+        Prev.next = delete.next;
+        return delete.data;
     }
-//删除指定的所有内容
+
     @Override
     public void removeAllKey(int key) {
-       Node prev = this.head;
-       Node cur = this.head.next;
-       while(cur != null){
-           if(cur.data == key){
-               prev.next = cur.next;
-               cur = cur.next;
-           } else{
-               prev = cur;
-               cur = cur.next;
-           }
-       }
+        if(this.head == null) {
+            return;
+        }
+        Node prev = this.head;
+        Node cur = this.head.next;
+        while (cur != null) {
+            if(cur.data == key){
+                prev.next = cur.next;
+                cur = prev.next;
+            }else {
+                prev = cur;
+                cur = cur.next;
+            }
+        }
+        if(this.head.data == key){
+            this.head = this.head.next;
+        }
     }
-//获取列表的长度
+
+    //获取列表的长度
     @Override
     public int getLength() {
         Node cur = this.head;
