ea554afbab26ad46362103bcd752acbe9348588b
handerman <2862551221@qq.com>
2019-06-24 21:38:05 +0800

    简单测试正确启动多线程

--- java_project/src/TextThread.java
+++ java_project/src/TextThread.java
@@ -14,9 +14,9 @@ public class TextThread {
         MyThread myThread = new MyThread("thread");
         MyThread myThread1 = new MyThread("thread1");
         MyThread myThread2 = new MyThread("thread2");
-        myThread.run();
-        myThread1.run();
-        myThread2.run();
+        myThread.start();
+        myThread1.start();
+        myThread2.start();
     }
 
 }
