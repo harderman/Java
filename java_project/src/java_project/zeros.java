296398bfb82fe2c3f2495c7be5d86af9846bfbb5
handerman <2862551221@qq.com>
2019-08-14 22:49:33 +0800

    zeros

---
+++ java_project/src/java_project/zeros.java
@@ -0,0 +1,20 @@
+package DayQuestion;
+
+import java.util.Scanner;
+
+public class zeros {
+    public static void main(String[] args) {
+        Scanner str = new Scanner(System.in);
+        int number = str.nextInt();
+        int resoult = 0;
+        for(int i = 5;i <= number;i++){
+            int temp = i;
+            while(temp%5 ==0){
+                resoult++;
+                temp /= 5;
+            }
+        }
+        System.out.println(resoult);
+    }
+}
+
